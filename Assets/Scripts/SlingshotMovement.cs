fd28a4887e92ebedf6259fc4565426f4b33a3846
Dorian Vabre <dorian.vabre@gmail.com>
2022-12-09 11:00:45 +0100

    v1 of player movement using gamepad

--- Assets/Scripts/SlingshotMovement.cs
+++ Assets/Scripts/SlingshotMovement.cs
@@ -1,14 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlingshotMovement : MonoBehaviour
 {
-    public float forceMultiplier = 50;
-    private Vector3 initialPosition;
-    private bool isDragging;
-
     Rigidbody2D rb;
+
+    public float forceMultiplier = 50;
+    public float deadZone = 0.02f;
+
+    private Vector2 currentMovementInput = Vector2.zero;
+    private Vector2 lastMovementInput = Vector2.zero; 
+
+    // FOR MOUSE ONLY, CAN BE DELETED AFTER CONTROLLER DONE
+    private Vector3 initialPosition;
+    private bool isDragging;   
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -20,14 +25,36 @@ public class SlingshotMovement : MonoBehaviour
 
     void Update()
     {
-        if (isDragging) {
-            // do stuff during drag
-        } else {
-            // Updating initial position
+        // FOR MOUSE ONLY, CAN BE DELETED AFTER CONTROLLER DONE
+        if (!isDragging) {
             initialPosition = transform.position;
         }
     }
 
+    public void OnMove(InputAction.CallbackContext context) {
+        currentMovementInput = context.ReadValue<Vector2>();
+        Debug.Log(currentMovementInput);
+        if (Mathf.Abs(currentMovementInput.x) <= deadZone && Mathf.Abs(currentMovementInput.y) <= deadZone) {
+            if (lastMovementInput.x != 0 && lastMovementInput.y != 0) {
+                Propel(lastMovementInput * -1);
+                lastMovementInput = Vector2.zero;
+            }
+        } else {
+            lastMovementInput = currentMovementInput;
+        }
+
+    }
+
+    // Applying the force to the rigidbody and resets the input
+    private void Propel(Vector2 force) {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0;
+        rb.AddForce(force * forceMultiplier);
+    }
+
+
+
+    // FOR MOUSE ONLY, CAN BE DELETED AFTER CONTROLLER DONE
     // Starts of a drag
     private void OnMouseDown() {
         GetComponent<SpriteRenderer>().color = Color.red;
@@ -36,17 +63,14 @@ public class SlingshotMovement : MonoBehaviour
         rb.angularVelocity = 0;
     }
 
+    // FOR MOUSE ONLY, CAN BE DELETED AFTER CONTROLLER DONE
     // On release, get the force and apply it with Propel()
     private void OnMouseUp() {
         GetComponent<SpriteRenderer>().color = Color.white;
         Vector3 draggedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 forceDirection = initialPosition - draggedPosition;
+        Vector2 forceDirection2d = new Vector2(forceDirection.x, forceDirection.y);
         Propel(forceDirection);
         isDragging = false;
-    }
-
-    // Applying the force to the rigidbody
-    private void Propel(Vector3 force) {
-        rb.AddForce(force * forceMultiplier);
     }
 }
