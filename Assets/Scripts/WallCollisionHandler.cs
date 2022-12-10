6b1241be78d38583fa9961d2ccb28fa56eecf595
fae.exe <fae.exe@pm.me>
2022-12-10 20:28:40 +0100

    Created a serializable Object Shaker class
    Takes an injected game object and shakes it using the access value of a wiggler. Serializable.

--- Assets/Scripts/WallCollisionHandler.cs
+++ Assets/Scripts/WallCollisionHandler.cs
@@ -114,6 +114,40 @@ using UnityEngine;
     }    
 }
 
+[Serializable] public class ObjectShaker {
+    // Shakes the position of an injectable game object according to the access value of a wiggler.
+    // Stores the origin position of the object and restores that position at the end of the wiggler's timer.
+
+    public Vector3Wiggler wiggler;
+    public GameObject objectToShake;
+    
+    public Vector3 originPosition;
+
+    public bool isActive = false;
+
+    public void LaunchShake () {
+        isActive = true;
+        originPosition = objectToShake.transform.position;
+        wiggler.LauchWiggler();
+    }
+
+    public void Update () {
+
+        if (!isActive) {
+            return;
+        }
+
+        if (!wiggler.isActive) {
+            isActive = false;
+            objectToShake.transform.position = originPosition;
+            return;
+        }
+
+        wiggler.Update();
+        objectToShake.transform.position = originPosition + wiggler.wiggleAccessValue;
+    }
+}
+
 public class WallCollisionHandler : MonoBehaviour
 {
     public AutoAnimator bumpAnimator;
