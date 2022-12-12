dd9e603f5a5ff80954748a401d5cc4e8a679f435
fae.exe <fae.exe@protonmail.com>
2022-12-12 12:41:00 +0100

    repaired the scene

---
+++ Assets/Scripts/Sling.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sling {
+
+    // Creates sling-like movements.
+    // The update method takes in a vector2.
+    // If the joystick is being pushed, it charges a power meter and calculates a vector access value based on that.
+    // If the power meter is full, it indicates that it must fire after a set duration. (That duration is a parameter.)
+    // If the joystick is in the deadzone, it also says it must fire after a set duration within the deadzone. (Parameter.)
+
+    [Header("Sling charge parameters")] [Space(3)]
+    public AnimationCurve chargeCurve;
+    public float timeToFullChargeSeconds;
+
+    [Header("Throw parameters")] [Space(3)]
+    public float strength;
+    public float deadzone;
+    public float deadzoneFireThreshold;
+    public float cooldownSeconds;
+
+    [Header("Throw trigger parameters")] [Space(3)]
+    public float timeInDeadzoneToTriggerSeconds;
+    public float timeAtFullChargeToTriggerSeconds;
+
+    public bool mustFire;
+
+    public float vectorMagnitude;
+
+    [SerializeField]
+    private float _timeSinceChargeStartSeconds;
+
+    [SerializeField]
+    private float _timeSinceFullChargeSeconds;
+
+    [SerializeField]
+    private float _timeSinceDeadZoneSeconds;
+
+    private bool _isInCooldown;
+    private float _timeInCooldownSeconds;
+
+    [System.NonSerialized] public Vector2 accessVector;
+    [System.NonSerialized] public float accessValue;
+    [System.NonSerialized] public float accessValueInv;
+
+    public void InputInDeadzone () {
+
+        _timeSinceDeadZoneSeconds += Time.deltaTime;
+
+        if (_timeSinceChargeStartSeconds/timeToFullChargeSeconds < deadzoneFireThreshold) {
+            _timeSinceChargeStartSeconds = Mathf.Max(_timeSinceChargeStartSeconds - Time.deltaTime, 0);
+            return;
+        }
+
+        if (_timeSinceDeadZoneSeconds > timeInDeadzoneToTriggerSeconds) {
+            mustFire = true;
+        }
+    }
+
+    public void SlingInCooldown () {
+
+        _timeInCooldownSeconds += Time.deltaTime;
+
+        if (_timeInCooldownSeconds > cooldownSeconds) {
+            _timeInCooldownSeconds = 0.0f;
+            _isInCooldown = false;
+        }
+    }
+
+    public void SlingAtFullCharge () {
+        _timeSinceFullChargeSeconds += Time.deltaTime;
+
+        if (_timeSinceFullChargeSeconds > timeAtFullChargeToTriggerSeconds) {
+            _timeSinceFullChargeSeconds = 0.0f;
+            mustFire = true;
+        }
+    }
+
+    public void Fire () {
+        _timeInCooldownSeconds = 0.0f;
+        _timeSinceChargeStartSeconds = 0.0f;
+        _timeSinceFullChargeSeconds = 0.0f;
+        _isInCooldown = true;
+        mustFire = false;
+    }
+
+    public void Update(Vector2 movementVector) {
+
+        vectorMagnitude = movementVector.magnitude;
+
+        if (_isInCooldown) {
+            SlingInCooldown ();
+            return;
+        }
+
+        if (vectorMagnitude < deadzone) {
+            InputInDeadzone ();
+            return;
+        }
+
+        _timeSinceDeadZoneSeconds = 0;
+
+        if (_timeSinceChargeStartSeconds >= timeToFullChargeSeconds) {
+            SlingAtFullCharge();
+        }
+        else {
+            _timeSinceChargeStartSeconds += Time.deltaTime;
+        }
+
+        accessValue = chargeCurve.Evaluate(_timeSinceChargeStartSeconds / timeToFullChargeSeconds);
+        accessValueInv = 1.0f - chargeCurve.Evaluate(_timeSinceChargeStartSeconds / timeToFullChargeSeconds);
+
+        accessVector = (-1.0f) * accessValue * strength * movementVector.normalized;
+    }
+
+}
--- Assets/Scripts/SlingshotMovement.cs
+++ Assets/Scripts/SlingshotMovement.cs
@@ -5,132 +5,29 @@ using UnityEngine.InputSystem;
 
 public class SlingshotMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
-    GameObject arrowLine;
-    GameObject arrowHead;
+    
+    public Rigidbody2D bacteriaRigidBody;
+    public Sling sling;
 
-    public AnimationCurve curve;
-    public float timeBetweenMovements = 0.5f;
-    public float currentTimeSinceLastMove;
+    private Vector2 _currentMovementInputVector = Vector2.zero;
 
-    public float forceMultiplier = 100;
-    public float deadZone = 0.02f;
+    public void OnMove (InputAction.CallbackContext context) {
 
-    private Vector2 currentMovementInput = Vector2.zero;
-    private Vector2 lastMovementInput = Vector2.zero;
-    public float minMagnitudeDeltaToTriggerPropel = 0.2f;
+        _currentMovementInputVector = context.ReadValue<Vector2>();
+    }
 
-    // FOR MOUSE ONLY, CAN BE DELETED AFTER CONTROLLER DONE
-    private Vector3 initialPosition;
-    private bool isDragging;
+    public void Update () {
+        sling.Update(_currentMovementInputVector);
 
-    private void Awake() {
-        rb = GetComponent<Rigidbody2D>();
-        foreach(Transform transform in transform) {
-            if (transform.name == "ArrowHead") {
-                arrowHead = transform.gameObject;
-            } else if (transform.name == "LineContainer") {
-                arrowLine = transform.gameObject;
-            }
+        if (sling.mustFire) {
+            sling.Fire();            
+            Propel();
         }
     }
 
-    void Start() {
-        isDragging = false;
-        arrowLine.SetActive(false);
-        arrowHead.SetActive(false);
-        currentTimeSinceLastMove = 0;
-    }
-
-    void Update()
-    {
-        currentTimeSinceLastMove += Time.deltaTime;
-
-        // FOR MOUSE ONLY, CAN BE DELETED AFTER CONTROLLER DONE
-        if (!isDragging) {
-            // do stuff during drag
-        } else {
-            // Updating initial position
-            initialPosition = transform.position;
-        }
-    }
-
-    public void OnMove(InputAction.CallbackContext context) {
-        if (currentTimeSinceLastMove < timeBetweenMovements) {
-            return;
-        }
-
-        currentMovementInput = context.ReadValue<Vector2>();
-        
-        // Moving the direction arrowLine
-        Vector2 vectorForDirectionArrow = currentMovementInput * -1;
-        float angle = Mathf.Atan2(vectorForDirectionArrow.y, vectorForDirectionArrow.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        // Scaling the line
-        Vector2 localScale = new Vector2(currentMovementInput.magnitude, 1);
-        arrowLine.transform.localScale = localScale;
-
-        // Showing the arrow
-        arrowLine.SetActive(true);
-        arrowHead.SetActive(true);
-
-        Vector2 deltaInput = currentMovementInput - lastMovementInput;
-
-        // Moving the bacteria
-        if (Vector2.Dot(deltaInput, currentMovementInput) <= 0) {
-            // Either the movement towards the idle joystick is enough OR the current movement is slow and back to idle
-            if (deltaInput.magnitude >= minMagnitudeDeltaToTriggerPropel || currentMovementInput.magnitude <= deadZone) {
-                arrowLine.SetActive(false);
-                arrowHead.SetActive(false);
-                if (lastMovementInput.x != 0 || lastMovementInput.y != 0) {
-                    Propel(lastMovementInput * -1);
-                    currentTimeSinceLastMove = 0;
-                    lastMovementInput = Vector2.zero;
-                }
-            } else {
-                lastMovementInput = currentMovementInput;
-            }
-        } else {
-            lastMovementInput = currentMovementInput;
-        }
-    }
-
-    // Applying the force to the rigidbody and resets the input
-    private void Propel(Vector2 force) {
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = 0;
-        float xMovementUsingCurve = curve.Evaluate(force.x);
-        float yMovementUsingCurve = curve.Evaluate(force.y);
-        Vector2 movementUsingCurve = new Vector2(xMovementUsingCurve, yMovementUsingCurve);
-        rb.AddForce(force * forceMultiplier);
-    }
-
-
-
-
-
-
-    // FOR MOUSE ONLY, CAN BE DELETED AFTER CONTROLLER DONE
-    // Starts of a drag
-    private void OnMouseDown() {
-        GetComponent<SpriteRenderer>().color = Color.red;
-        isDragging = true;
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = 0;
-    }
-
-    // On release, get the force and apply it with Propel()
-    private void OnMouseUp() {
-        GetComponent<SpriteRenderer>().color = Color.white;
-        Vector3 draggedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 forceDirection = initialPosition - draggedPosition;
-        Propel(forceDirection);
-        isDragging = false;
-    }
-
-    // Applying the force to the rigidbody
-    private void Propel(Vector3 force) {
-        rb.AddForce(force * forceMultiplier);
+    public void Propel () {
+        bacteriaRigidBody.velocity = Vector3.zero;
+        bacteriaRigidBody.angularVelocity = 0;
+        bacteriaRigidBody.AddForce(sling.accessVector);
     }
 }
