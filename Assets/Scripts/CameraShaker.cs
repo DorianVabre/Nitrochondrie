484b5d871831bba7344ad76322008c49f18192f4
fae.exe <fae.exe@protonmail.com>
2022-12-13 14:03:36 +0100

    Added repeat decay to camera shake

--- Assets/Scripts/CameraShaker.cs
+++ Assets/Scripts/CameraShaker.cs
@@ -6,6 +6,15 @@ public class CameraShaker : MonoBehaviour
 {
     public Vector3Wiggler wiggler;
     public GameObject cameraHolder;
+
+    public float cooldownDuration;
+    private float _cooldownTimer;
+    private bool _onCooldown;
+
+    public float intensityDecayerIntervalSeconds;
+    private float _intensityDecayerSeconds;
+
+    public AnimationCurve intensityMuterCurve;
     
     public Vector3 originPosition;
 
@@ -15,18 +24,47 @@ public class CameraShaker : MonoBehaviour
         originPosition = cameraHolder.transform.position;
     }
 
+    public void Cooldown () {
+        _cooldownTimer += Time.deltaTime;
+        
+        if (_cooldownTimer > cooldownDuration) {
+            _cooldownTimer = 0.0f;
+            _onCooldown = false;
+            return;
+        }
+    }
+
+    public void addIntensityDecay() {
+        _intensityDecayerSeconds = Mathf.Min( _intensityDecayerSeconds + intensityDecayerIntervalSeconds, 3.0f);
+    }
+
     public void LaunchShake (float intensityParameter = 0.0f) {
+
+        if (_onCooldown) {
+            return;
+        }
+
+        _onCooldown = true;
+
+        
+        wiggler.LaunchWiggler(intensityParameter);
+        addIntensityDecay();
+        
         if (isShaking) {
-            wiggler.LaunchWiggler(intensityParameter);
             return;
         }
 
         isShaking = true;
         originPosition = cameraHolder.transform.position;
-        wiggler.LaunchWiggler(intensityParameter);
     }
 
     public void LateUpdate () {
+        
+        _intensityDecayerSeconds = Mathf.Max(0, _intensityDecayerSeconds -= Time.deltaTime);
+
+        if (_onCooldown) {
+            Cooldown ();
+        }
 
         if (!isShaking) {
             return;
@@ -39,6 +77,8 @@ public class CameraShaker : MonoBehaviour
         }
 
         wiggler.Update();
-        cameraHolder.transform.position = originPosition + wiggler.wiggleAccessValue;
+
+        cameraHolder.transform.position = originPosition
+        + wiggler.wiggleAccessValue * intensityMuterCurve.Evaluate(_intensityDecayerSeconds);
     }
 }
