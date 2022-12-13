aa404309787463225475a570f729ffe4587fc016
Nathanaël <nathanael.broch@yahoo.fr>
2022-12-13 14:25:31 +0100

    Added checkpoint music script and fixed transition for main level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] public Transform playerOne;
-    [SerializeField] public Transform playerTwo;
+    [SerializeField] private Transform playerOne;
+    [SerializeField] private Transform playerTwo;
+
+    [SerializeField] private AudioSource[] lapMusic;
 
     // Start is called before the first frame update
     void Start() {
-
+        lapMusic[0].volume = 1f;
+        for (int i = 1; i < lapMusic.GetLength(1); i++){
+            lapMusic[i].volume = 0f;
+        }
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    public void CheckMusicChange(int lapsDoneByP1, int lapsDoneByP2,
+                                 int  checkpointsReachedByP1, int checkpointsReachedByP2){
+        IncrementMusic(new Vector2(
+            Mathf.Max(lapsDoneByP1, lapsDoneByP2),
+            Mathf.Max(checkpointsReachedByP1, checkpointsReachedByP2)
+        ));
+    }
+
+    private void IncrementMusic(Vector2 bestCheckpoint){
+        if(bestCheckpoint.x >= 1f){
+            lapMusic[(int)bestCheckpoint.x].volume = 0.34f * bestCheckpoint.y;
+        }
     }
 }
